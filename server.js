ae786e3f5b5425d0cf2cf5b4294bbf6378b9dd2a
BenedyktT <benedykt.tyminski@gmail.com>
2020-03-29 18:32:33 +0000

    heroku build

--- server.js
+++ server.js
@@ -9,5 +9,8 @@ app.use(express.json({ extended: false }));
 
 app.use("/recipes", require("./api/recipes"));
 app.use("/save", require("./api/save"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 module.exports = app;
