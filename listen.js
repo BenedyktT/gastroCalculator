b875e69239be55afb7fc67be301c268a8f99118c
BenedyktT <benedykt.tyminski@gmail.com>
2020-03-29 19:32:30 +0000

    fix styling

--- listen.js
+++ listen.js
@@ -1,5 +1,8 @@
 const app = require("./server");
 const PORT = process.env.PORT || 5000;
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 app.listen(PORT, (req, res) => {
   console.log(`server is working on port ${PORT}`);
 });
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV !== "production") require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const connectDb = require("./config/connectDb");
@@ -9,8 +9,5 @@ app.use(express.json({ extended: false }));
 
 app.use("/recipes", require("./api/recipes"));
 app.use("/save", require("./api/save"));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
 
 module.exports = app;
