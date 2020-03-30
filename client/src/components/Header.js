f2e8925664ea1588ddf40b00dda3dc52433481ad
BenedyktT <benedykt.tyminski@gmail.com>
2020-03-30 20:11:21 +0000

    styling issue fix

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,8 +9,14 @@ const Header = () => {
           <h1>Gastro Calculator</h1>
         </Link>
         <nav className="nav">
-          <Link to="/">Home</Link>
-          <Link to="/recipes">Browse Recipes</Link>
+          <div className="nav__element nav__element--home">
+            {" "}
+            <Link to="/">Home</Link>
+          </div>
+          <div className="nav__element">
+            {" "}
+            <Link to="/recipes">Browse Recipes</Link>
+          </div>
         </nav>
       </header>
     </div>
