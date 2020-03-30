d92adf100d384427ea6da4df464acf04cd2c67ed
BenedyktT <benedykt.tyminski@gmail.com>
2020-03-30 19:12:38 +0000

    heroku deploy bugfix

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const app = express();
 const connectDb = require("./config/connectDb");
-const path = require('path')
-
+const path = require("path");
+const root = require("path").join(__dirname, "client", "build");
 connectDb.connectDB();
 
 app.use(express.json({ extended: false }));
 
 app.use("/recipes", require("./api/recipes"));
 app.use("/save", require("./api/save"));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client" , "build", "index.html"));
-});
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(root));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 module.exports = app;
