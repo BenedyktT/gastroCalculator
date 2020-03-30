a631101398385150c517510524393225d42bc584
Benedykt <32865753+BenedyktT@users.noreply.github.com>
2020-03-30 16:52:45 +0000

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const connectDb = require("./config/connectDb");
+const path = require('path')
 
 connectDb.connectDB();
 
@@ -9,5 +10,8 @@ app.use(express.json({ extended: false }));
 
 app.use("/recipes", require("./api/recipes"));
 app.use("/save", require("./api/save"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 module.exports = app;
