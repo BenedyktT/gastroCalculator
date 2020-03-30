5bfa0419de32d63736a860d0eaa034d7ef7ced23
Benedykt <32865753+BenedyktT@users.noreply.github.com>
2020-03-30 16:57:08 +0000

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json({ extended: false }));
 app.use("/recipes", require("./api/recipes"));
 app.use("/save", require("./api/save"));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "client" , "build", "index.html"));
 });
 
 module.exports = app;
