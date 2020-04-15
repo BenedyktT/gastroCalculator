6489eaecd469001e50c79de12b727daf8db15afc
BenedyktT <benedykt.tyminski@gmail.com>
2020-04-15 10:06:29 +0000

    redis url

--- src/config/index.js
+++ src/config/index.js
@@ -2,5 +2,5 @@ module.exports = {
   apiKey: process.env.API_KEY,
   apiId: process.env.API_ID,
   dbKey: process.env.MONGO_URI,
-  redisUrl: "redis://127.0.0.1:6379",
+  redisUrl: process.env.REDIS_URL || "redis://127.0.0.1:6379",
 };
